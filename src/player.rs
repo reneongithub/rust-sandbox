9d5be3646a5d285ff00d5ea7d09c2f623ee51aca
Rene Kuehnemann <info@rene.ubuntudev.de>
2022-06-29 16:48:35 +0200

    structured play arround

--- src/player.rs
+++ src/player.rs
@@ -6,11 +6,61 @@ pub fn play_around(){
 
     println!("ENTER play_around\n");
 
+    test_misc();
+
+    test_pointer_on_string();
+
+    test_ordering();
+
+    test_use_keyword();
+
+    test_vector();
+
+
+    println!("EO play_around");
+}
+
+fn test_misc(){
+
+    println!("test misc__________________________");
+
     print_to_console(2, &String::from("to console"));
 
     let mut owned_string: String = "hello ".to_owned();
     
     owned_string.push_str(" there");
+
+
+
+
+    let opt_string = get_string_by_option(&owned_string).unwrap();
+
+    println!("got that from getStringByOption: {}", opt_string);
+
+    println!("EOT misc__________________________");
+
+}
+
+fn test_pointer_on_string(){
+    println!("test pointer_on_string__________________________");
+
+
+    let mut str = String::from("string to trans;");
+
+    let str_pt = &mut str;
+
+    str_pt.push_str("string;");
+
+    extend_string(str_pt);
+
+    println!("{}",str);
+    println!("EOT pointer_on_string__________________________");
+
+}
+
+
+fn test_ordering() {
+    println!("test ordering__________________________");
 
     let int_a = 1;
 
@@ -30,30 +80,50 @@ pub fn play_around(){
         println!("Equal from if condition == ")
     }
 
-    let opt_string = get_string_by_option(&owned_string).unwrap();
+    println!("EOT ordering__________________________");
+}
 
-    println!("got that from getStringByOption: {}", opt_string);
-
-    println!("EO play_around");
-
-    let mut str = String::from("string to trans;");
-
-    let str_pt = &mut str;
-
-    str_pt.push_str("string;");
-
-    extend_string(str_pt);
-
-    println!("{}",str);
-
-
-
+fn test_use_keyword() {
+    println!("test use_keyword__________________________");
 
     business::do_some_business();
     business::helper::help_me("Rina");
     business::helper_exp::help_me("Dane");
     business::helper_help_me_exp("Ralf");
+
+    println!("EOT use_keyword__________________________");
 }
+
+fn test_vector(){
+
+    println!("test vector__________________________");
+
+    let mut vec = Vec::new();
+
+    vec.push("one");
+    vec.push("tow");
+    vec.push("three");
+
+    for str in &vec {
+        println!("{}", str);
+    }
+
+    let val1 = vec.get(1);
+    match val1 {
+
+        Some(v) => println!("{}",v),
+        _ => ()
+        
+    }
+
+    if let Some(v) = val1 {
+        println!("{}",v);
+    }
+
+    println!("EOT vector__________________________")
+
+}
+
 
 fn print_to_console(int_to_console: i32, string_to_console: &String){
 
