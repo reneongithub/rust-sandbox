308371b1bc2286ececfeb7d0d3cad2d7ff2b92c9
rene kuehnemann <kuehnemannrene@gmail.com>
2024-07-24 18:23:47 +0200

    [phmcom] test hello command

--- phmcom-poc/phmcom-cli/src/main.rs
+++ phmcom-poc/phmcom-cli/src/main.rs
@@ -1,3 +1,17 @@
+use clap::{Arg, Command};
+
 fn main() {
-    println!("Hello, World!");
+    let command = Command::new("Dog Shelter sample application")
+        .version("1.0")
+        .author("Rene Kuehnemann <rene@wmedia.de>")
+        .about("Say hello poc")
+        .arg(
+            Arg::new("config")
+                .short('c')
+                .long("config")
+                .help("Configuration file location")
+                .default_value("config.json"),
+        );
+
+    _ = command.get_matches();
 }
